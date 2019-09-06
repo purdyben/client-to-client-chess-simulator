e49a3b72e2f7ad82c726ef400cd3623308a3099c
CMaster20 <colbym@iastate.edu>
2019-09-06 10:35:36 -0500

    initalizing spring experiment

---
+++ Experiments/colby/FastCare/src/main/java/com/fastcare/src/FastCareApplication.java
@@ -0,0 +1,13 @@
+package com.fastcare.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCareApplication.class, args);
+	}
+
+}
