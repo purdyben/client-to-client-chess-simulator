a3f025dbc40cd3bdbe3876f8841469145c59df92
CMaster20 <colbym@iastate.edu>
2019-09-07 20:03:45 -0500

    adding tests

--- Experiments/colby/FastCare/src/main/java/com/fastcare/hello/HelloController.java
+++ Experiments/colby/FastCare/src/main/java/com/fastcare/hello/HelloController.java
@@ -1,16 +1,22 @@
 package com.fastcare.hello;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	@RequestMapping("/")
-	public String Welcom() {
-		return "Welcome to FastCare!";
-	}
+	
 	@RequestMapping("/hello")
 	public String HelloWorld() {
 		return "Hello, World!";
 	}
+	
+	@GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
