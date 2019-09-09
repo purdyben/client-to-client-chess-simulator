970d8ed44c853325841943897580ec1ce6a1452a
CMaster20 <colbym@iastate.edu>
2019-09-09 15:54:47 -0500

    trying to add repo controller

--- Experiments/colby/FastCare/src/main/java/com/fastcare/users/User.java
+++ Experiments/colby/FastCare/src/main/java/com/fastcare/users/User.java
@@ -1,7 +1,11 @@
 package com.fastcare.users;
 
+import org.springframework.data.annotation.Id;
+
 public class User {
 
+	@Id
+	private long id;
 	private String name = "";
 	public User(String name) {
 		this.name = name;
---
+++ Experiments/colby/FastCare/src/main/java/com/fastcare/users/UserRepository.java
@@ -0,0 +1,8 @@
+package com.fastcare.users;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository extends CrudRepository<User, Long>  {
+
+}
