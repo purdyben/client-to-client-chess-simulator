a93270ff5fd26d8290b4cc40acdbffa5965e0e8f
EllissaPeterson <ellissa@iastate.edu>
2019-09-17 09:35:10 -0500

    React router setup

--- Experiments/ellissa/pirate-chess/src/App.js
+++ Experiments/ellissa/pirate-chess/src/App.js
@@ -1,19 +1,55 @@
 import React from 'react';
 import './App.css';
 import {Button, NavLink} from 'reactstrap';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Home = () => (
+  <header className="App-header">
+    <img src="logo.png" className="App-logo" alt="logo" />
+      <div className="Login-button">
+        <Button color="primary" size="lg" href="/login" block>Login</Button>
+        <Button color="secondary" size="lg" href="/guestLogin"block>Guest Login</Button>
+      </div>
+      <NavLink href="/createAccount" classname="Create-account-link">Create Account</NavLink>
+  </header>
+)
+
+const Login = () => (
+  <div>
+    Login
+  </div>
+)
+
+const GuestLogin = () => (
+  <div>
+    GuestLogin
+  </div>
+)
+
+const CreateAccount = () => (
+  <div>
+    CreateAccount
+  </div>
+)
+
+const Game = () => (
+  <div>
+    Game
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="logo.png" className="App-logo" alt="logo" />
-        <div className="Login-button">
-          <Button color="primary" size="lg" block>Login</Button>
-          <Button color="secondary" size="lg" block>Guest Login</Button>
-        </div>
-        <NavLink href="" classname="Create-account-link">Create Account</NavLink>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/guestLogin" component={GuestLogin} />
+          <Route exact path="/createAccount" component={CreateAccount} />
+          <Route exact path="/game" component={Game} />
+      </div>
+    </Router>
   );
 }
 
