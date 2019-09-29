01c6b4fcda3b56a487d3e340c6d3375db4e63da5
CMaster20 <colbym@iastate.edu>
2019-09-29 06:13:05 -0500

    adding ranks things

--- BackEnd/PirateChess/src/main/java/com/piratechess/user/UserService.java
+++ BackEnd/PirateChess/src/main/java/com/piratechess/user/UserService.java
@@ -1,23 +1,67 @@
 package com.piratechess.user;
 
 public class UserService {
-	
+
+	/**
+	 * Updates both players rankings after a game has been played
+	 * 
+	 * @author Colby McKinley
+	 * @param a      - player 1
+	 * @param b      - player 2
+	 * @param winner
+	 * @see https://en.wikipedia.org/wiki/Elo_rating_system
+	 */
 	public void updateRank(User a, User b, User winner) {
-		int currentA = a.getRankScore();
-		int currentB = b.getRankScore();
-		int newScoreA = currentA + currentB;
+		int newScoreA = a.getRankScore() + b.getRankScore();
 		int newScoreB = newScoreA;
-		if(a.equals(winner)) {
+		if (a.equals(winner)) {
 			newScoreA += 400;
 			newScoreB -= 400;
-			
-		}else {
+
+		} else {
 			newScoreA -= 400;
 			newScoreB += 400;
 		}
 		newScoreA /= a.getNumGames();
 		newScoreB /= b.getNumGames();
 		a.setRankScore(newScoreA);
+		a.setClassification(getRankByScore(newScoreA));
 		b.setRankScore(newScoreB);
+		b.setClassification(getRankByScore(newScoreB));
+
+	}
+
+	/**
+	 * 
+	 * @param score
+	 * @return
+	 */
+	public String getRankByScore(int score) {
+		if (score >= 2400) {
+			return "Senior Master";
+		} else if (score >= 2200) {
+			return "National Master";
+		} else if (score >= 2000) {
+			return "Expert";
+		} else if (score >= 1800) {
+			return "Class A";
+		} else if (score >= 1600) {
+			return "Class B";
+		} else if (score >= 1400) {
+			return "Class C";
+		} else if (score >= 1200) {
+			return "Class D";
+		} else if (score >= 1000) {
+			return "Class E";
+		} else if (score >= 800) {
+			return "Class F";
+		} else if (score >= 600) {
+			return "Class G";
+		} else if (score >= 400) {
+			return "Class H";
+		} else if (score >= 200) {
+			return "Class I";
+		}
+		return "Class J";
 	}
 }
