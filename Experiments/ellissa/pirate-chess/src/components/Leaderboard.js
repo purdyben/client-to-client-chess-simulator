c49016c99e6fda5b27d9528494b8cf02ba207e1a
EllissaPeterson <ellissa@iastate.edu>
2019-09-29 20:59:49 -0500

    Revert "leaderboard stuff"
    This reverts commit 48af9ccde99195883cdc7de55494bbb10b483b8c.

--- Experiments/ellissa/pirate-chess/src/components/Leaderboard.js
+++ Experiments/ellissa/pirate-chess/src/components/Leaderboard.js
@@ -1,21 +1,10 @@
 import React from 'react';
 
 const Leaderboard = () => (
-  <div>
-    <div className="Navigation-bar">
-      <ButtonGroup vertical size="lg">
-          <Button color="primary" href="/mainScreen">Main Screen</Button>
-          <Button color="primary" href="/game">Play Now</Button>
-          <Button color="primary" href="/leaderboard">Chat</Button>
-          <Button color="primary" href="/howToPlay">How To Play</Button>
-          <Button color="primary" href="/tournament">Tournament</Button>
-        </ButtonGroup>
-    </div>
     <header className="App-header">
       Leaderboard
     </header>
-  </div>
-)
+  )
 
 export default Leaderboard;
   
