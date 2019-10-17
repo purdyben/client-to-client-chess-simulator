df1d2f8ff51fa59c336a33da8d296cb3bfbe2399
CMaster20 <colbym@iastate.edu>
2019-10-16 21:26:47 -0500

    somethign i forgot dealing iwth rss feeds

--- BackEnd/PirateChess/src/main/java/com/piratechess/news/CustomRssViewer.java
+++ BackEnd/PirateChess/src/main/java/com/piratechess/news/CustomRssViewer.java
@@ -1,5 +1,4 @@
 package com.piratechess.news;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,49 +7,46 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.view.feed.AbstractRssFeedView;
 
-import com.sun.syndication.feed.rss.Channel;
-import com.sun.syndication.feed.rss.Content;
-import com.sun.syndication.feed.rss.Item;
-
 public class CustomRssViewer extends AbstractRssFeedView {
 
-	@Override
-	protected void buildFeedMetadata(Map<String, Object> model, Channel feed,
-		HttpServletRequest request) {
-		
-		feed.setTitle("Mkyong Dot Com");
-		feed.setDescription("Java Tutorials and Examples");
-		feed.setLink("http://www.mkyong.com");
-		
-		super.buildFeedMetadata(model, feed, request);
-	}
+//	@Override
+//	protected void buildFeedMetadata(Map<String, Object> model, Channel feed,
+//		HttpServletRequest request) {
+//		
+//		feed.setTitle("Mkyong Dot Com");
+//		feed.setDescription("Java Tutorials and Examples");
+//		feed.setLink("http://www.mkyong.com");
+//		
+//		super.buildFeedMetadata(model, feed, request);
+//	}
 	
 	
 	@Override
-	protected List<RSSObject> buildFeedItems(Map<String, Object> model,
+	protected List<com.rometools.rome.feed.rss.Item> buildFeedItems(Map<String, Object> model,
 		HttpServletRequest request, HttpServletResponse response)
 		throws Exception {
-		
-		@SuppressWarnings("unchecked")
-		List<RSSObject> listContent = (List<RSSObject>) model.get("feedContent");
-		List<Item> items = new ArrayList<Item>(listContent.size());
-		
-		for(RSSObject tempContent : listContent ){
-		
-			Item item = new Item();
-			
-			Content content = new Content();
-			content.setValue(tempContent.getSummary());
-			item.setContent(content);
-			
-			item.setTitle(tempContent.getTitle());
-			item.setLink(tempContent.getUrl());
-			item.setPubDate(tempContent.getCreatedDate());
-			
-			items.add(item);
-		}
-		
-		return items;
-	}
+//		
+//		@SuppressWarnings("unchecked")
+//		List<RSSObject> listContent = (List<RSSObject>) model.get("feedContent");
+//		List<RSSObject> items = new ArrayList<RSSObject>(listContent.size());
+//		
+//		for(RSSObject tempContent : listContent ){
+//		
+//			Item item = new Item();
+//			
+//			Content content = new Content();
+//			content.setValue(tempContent.getSummary());
+//			item.setContent(content);
+//			
+//			item.setTitle(tempContent.getTitle());
+//			item.setLink(tempContent.getUrl());
+//			item.setPubDate(tempContent.getCreatedDate());
+//			
+//			items.add(item);
+//		}
+//		
+//		return items;
+			return null;
+}
 
 }
