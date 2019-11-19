03c2d7b41d2ff7006b0560a2b3cbd021990895d0
CMaster20 <colbym@iastate.edu>
2019-11-18 22:00:36 -0600

    adding ai service

---
+++ BackEnd/PirateChess/src/main/java/com/piratechess/play/AIService.java
@@ -0,0 +1,10 @@
+package com.piratechess.play;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AIService {
+	public static String processMove(String move) {
+		return "e4";
+	}
+}
